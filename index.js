1e00fb80259cd59c5fc80a815f89b556952b091f
levanquang123 <levanquang27122005@gmail.com>
2026-03-17 16:10:43 +0700

    fix(index): add exposedHeaders

--- index.js
+++ index.js
@@ -18,7 +18,8 @@ if (!URL) {
 app.use(cors({
   origin: "*",
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
+  allowedHeaders: ["Content-Type", "Authorization", "Accept"], 
+  exposedHeaders: ["Content-Length"], 
 }));
 app.use(express.json()); 
 app.use(express.urlencoded({ extended: true }));
